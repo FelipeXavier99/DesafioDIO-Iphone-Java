4c9927628b7e8cb835dcc83f8b9d9968446c6ea0
Felipe Xavier <127893679+FelipeXavier99@users.noreply.github.com>
2023-09-20 14:08:31 -0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,1 +1,32 @@
+import telefone.Contato;
+import telefone.Iphone;
+
+public class Teste {
+
+        public static void main(String[] args) {
+            Iphone iphone = new Iphone("77777", "iPhone 12");
+
+            iphone.ligar();
+          
+            iphone.tocarMusica("Música Teste");
+            iphone.pausarMusica();
+           
+            iphone.selecionarMusica("Música Eletronica ");
+            iphone.excluirMusica("Música Rock");
+            iphone.adicionarMusica("Música Sertanejo");
+         
+    
+
+            iphone.atender();;
+            iphone.buscarContatos("Xaier");
+            Contato contatoParaChamada = new Contato("Felipe", "12345-6789");
+            iphone.ligarChamada(contatoParaChamada);
+            
+           -------------------------------");
+
+       
+
+            iphone.desligar();
+        }
+    }
 
