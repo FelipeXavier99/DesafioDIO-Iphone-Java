cae7473aa692f6facd464bc8a74af969854a4fc5
Felipe Xavier <127893679+FelipeXavier99@users.noreply.github.com>
2023-09-20 14:26:57 -0300

    Update and rename Reproduto.java to Reprodutor.java

---
+++ src/main/controller/Reprodutor.java
@@ -0,0 +1,12 @@
+package controller;
+
+public interface ReprodutorMusical {
+    void tocarMusica(String musica);
+    void pausarMusica();
+    void pausarVideo();
+    void selecionarMusica(String musica)
+    void excluirMusica(String musica);
+    void adicionarMusica(String musica);
+  
+
+}
