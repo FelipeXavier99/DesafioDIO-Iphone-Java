720eb6c49f257a5bcf03fb29155eada204d6dd86
Felipe Xavier <127893679+FelipeXavier99@users.noreply.github.com>
2023-09-20 14:28:03 -0300

    Create Telefone.java

---
+++ src/main/controller/Telefone.java
@@ -0,0 +1,13 @@
+package controller;
+
+public interface AparelhoTelefonico {
+    void ligarChamada(Contato contato);
+    void desligarChamada();
+    void atender();
+    void buscarContatos(String nome);
+    void excluirContato(String nome);
+    void adicionarContato(Contato contato);
+    void enviarSMS(String mensagem, Contato contato);
+    void iniciarCorreioVoz();
+
+}
